e74354411c9ee1a8b5c434aabf7a50060680185a
myahudi04sm <122536895+myahudi04sm@users.noreply.github.com>
2023-01-12 16:03:12 +0300

    Add files via upload

---
+++ Hello.c
@@ -0,0 +1,11 @@
+
+    
+//First program in c
+#include<stdio.h>
+
+int main () {
+    printf("Hello Word");
+    
+    return 0;
+    
+}
---
+++ Sam.c
@@ -0,0 +1,9 @@
+//First program in c
+#include<stdio.h>
+
+int main () {
+    printf("Sam Myahudi");
+    
+    return 0;
+    
+}
