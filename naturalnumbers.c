93632fac76e725d4dd69e15533b975980130bbd2
myahudi04sm <122536895+myahudi04sm@users.noreply.github.com>
2023-02-21 10:45:25 +0300

    Add files via upload

---
+++ naturalnumbers.c
@@ -0,0 +1,11 @@
+//Natural numbers
+#include <stdio.h>
+int main(){
+    int n;
+    for (n==1; n<=50; n++){
+        printf("%d\n", n);
+    }
+    
+    return 0;
+}
+
---
+++ sum natural nunbers.c
@@ -0,0 +1,11 @@
+//Sum of natural numbers
+#include <stdio.h>
+int main(){
+    int n;
+    int sum=1+2+3+4+5+6+7+8+9+10;
+    for (n==1; n<=100; n++){
+        printf("the sum is %d\n", sum);
+    }
+    
+    return 0;
+}
