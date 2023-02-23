32723015cfc267d8cbc39e2f0aa1e63914958f46
myahudi04sm <122536895+myahudi04sm@users.noreply.github.com>
2023-02-23 12:45:27 +0300

    Add files via upload

---
+++ cube.c
@@ -0,0 +1,11 @@
+//Cube of natural numbers
+#include <stdio.h>
+int main()
+{
+    int n;
+    for (n=1; n<=5; n++){
+    printf("number is:%d and cube is %d\n" ,n,n*n*n);
+    }
+    
+    return 0;
+}
